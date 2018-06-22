5de0e353d72f02fea19cb940abc96ecd764feef2
Robert Catmull <catmulltrim@gmail.com>
2018-06-22 07:54:58 -0600

    Adding Redis As Storage

--- app/services/cache/cache.go
+++ app/services/cache/cache.go
@@ -8,7 +8,7 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var redis_client *redis.Client
+var redisClient *redis.Client
 
 // InitializeRedisClient Initializes the Redis Client from Environment variables.
 func InitializeRedisClient() {
@@ -19,13 +19,13 @@ func InitializeRedisClient() {
 		db = 0
 	}
 
-	redis_client = redis.NewClient(&redis.Options{
+	redisClient = redis.NewClient(&redis.Options{
 		Addr:os.Getenv("REDIS_URL"),
 		Password:os.Getenv("REDIS_PASSWORD"),
 		DB:db,
 	})
 
-	ping, err := redis_client.Ping().Result()
+	ping, err := redisClient.Ping().Result()
 
 	if err != nil {
 		fmt.Println("Failed to Initialize Redis, " + ping)
@@ -43,12 +43,12 @@ func Get(queue string) ([]byte, error) {
 }
 
 func writeFile(key string, message []byte) error {
-	r := redis_client.Set(fmt.Sprintf("%v_%v", key, makeTimestamp()), message, 48 * time.Hour)
+	r := redisClient.Set(fmt.Sprintf("%v_%v", key, makeTimestamp()), message, 48 * time.Hour)
 	return r.Err()
 }
 
 func readFile(key string) ([]byte, error) {
-	iter := redis_client.Scan(0, key + "_*",1).Iterator()
+	iter := redisClient.Scan(0, key + "_*",1).Iterator()
 	iter.Next()
 
 	msg := []byte{}
@@ -57,9 +57,9 @@ func readFile(key string) ([]byte, error) {
 		return msg, iter.Err()
 	}
 
-	msg = []byte(redis_client.Get(iter.Val()).Val())
+	msg = []byte(redisClient.Get(iter.Val()).Val())
 
-	redis_client.Del(iter.Val())
+	redisClient.Del(iter.Val())
 
 	return msg, nil
 }
